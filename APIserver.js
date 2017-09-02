b39c4790f7b47e452082ab2f905510717e4f6e9b
Abhishek Chowdhary <abhishek.chy17@gmail.com>
2017-09-02 22:16:54 +0700

    Modify API Server

--- APIserver.js
+++ APIserver.js
@@ -11,7 +11,7 @@ var config = {
     database: 'imad-articles',
     host: "localhost",
     port:'5432',
-    password: 'xxxx'
+    password: "xxxx"
 };
 
 var app = express();
@@ -22,57 +22,8 @@ app.use(session({
     cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-function createTemplate(data){
-  var title = data.title;
-  var heading = data.heading;
-  var date = data.date;
-  var content = data.content;
-  var htmlTemplate = `
-        <!doctype html>
-        <html>
-          <head>
-            <meta name=viewport content="width=device-width initial-scale=1">
-            <title>${title}</title>
-            <link href="/ui/style.css" rel="stylesheet" />
-          </head>
-          <body>
-              <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <div>
-                    <h2>${heading}</h2>
-                </div>
-                
-                <div>
-                    ${date.toDateString()}
-                </div>
-                
-                <div>
-                    ${content}
-                </div>
-            </div>
-            <div class="footer center">
-            <button id="counter">Like</button><span id="count">0</span>
-            <div class="footer">
-                    <input type="text" id="comment" placeholder="add comment"/>
-                    <input type="submit" value ="Submit" id="submit_btn"/>
-                    <ul id="commentList">
-                    </ul>
-            </div>
-            </div>
-            <script type="text/javascript" src="/ui/main.js"></script>
-            <script type="text/javascript" src="/ui/article.js"></script>
-          </body>
-        </html>
-        `;
-        return htmlTemplate;
-
-};
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send("Android API Server");
 });
 
 function hash(input, salt){
@@ -161,13 +112,15 @@ app.get('/logout', function(req, res){
 })
 
 var pool = new Pool(config);
-app.get('/test-db', function(req, res){
+
+app.get('/get-articles', function(req, res){
    //make a select request
    //return a response with the results
-   pool.query('SELECT * from test', function(err, result){
+   pool.query('SELECT * from public.articles order by id', function(err, result){
       if (err){
           res.status(500).send(err.toString());
       } else{
+          res.setHeader('Content-Type', 'application/json');	
           res.send(JSON.stringify(result.rows));
       }
    });
@@ -191,7 +144,9 @@ app.get('/article/:articleName', function (req,res){
                 res.status(404).send('Article not found')
             } else{
                 var articleData = result.rows[0];
-                res.send(createTemplate(articleData));
+                res.setHeader('Content-Type', 'application/json');	
+                res.send(JSON.stringify(articleData));
+
             }
         }
     });
@@ -199,25 +154,10 @@ app.get('/article/:articleName', function (req,res){
     
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/ui/main.js', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-app.get('/ui/article.js', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article.js'));
-});
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 8080;
+var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
